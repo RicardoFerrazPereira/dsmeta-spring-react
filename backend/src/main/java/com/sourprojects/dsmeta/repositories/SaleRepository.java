2e995ce7a025dfe25850f0fe55c04157b5c3be13
RicardoFerraz <sourvivens@gmail.com>
2022-09-07 23:27:18 -0300

    Consulta por Data

--- backend/src/main/java/com/sourprojects/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/sourprojects/dsmeta/repositories/SaleRepository.java
@@ -1,11 +1,21 @@
 package com.sourprojects.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sourprojects.dsmeta.entities.Sale;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+	// função que faz uma pesquisa no banco de dados recebendo
+	// as datas como argumentos
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
--- backend/src/main/java/com/sourprojects/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/sourprojects/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.sourprojects.dsmeta.services;
 
-import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.sourprojects.dsmeta.entities.Sale;
@@ -14,8 +19,24 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale>findSales() {
-		return repository.findAll();		
+//	public List<Sale>findSales() {
+//		return repository.findAll();		
+//	}
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		// Variável com a da de hoje (atual)
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		// converter a data de string para LocalDate e se não for informado a data, colocar
+		// o a data de 1 ano (365 dias) atrás
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		// se a maxDate for igual a vazio ? coloque o "today" 
+		// senão coloque LocalDate.parse(maxDate)
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);		
+		
+		return repository.findSales(min, max, pageable);
+		
 	}
 
 }
