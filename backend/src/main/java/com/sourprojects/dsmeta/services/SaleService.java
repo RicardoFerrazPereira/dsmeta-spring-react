4591acfa5008f84f31f045a93066dce243ad1c51
RicardoFerraz <sourvivens@gmail.com>
2022-09-07 22:39:23 -0300

    API teste

---
+++ backend/src/main/java/com/sourprojects/dsmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.sourprojects.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sourprojects.dsmeta.entities.Sale;
+import com.sourprojects.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale>findSales() {
+		return repository.findAll();		
+	}
+
+}
