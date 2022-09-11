4b044bd50dc441e70af96523af830812252d7550
RicardoFerraz <sourvivens@gmail.com>
2022-09-11 09:26:19 -0300

    Lista de Venda

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 
 import NotificationButton from "../NotificationButton";
 import "./styles.css";
@@ -14,9 +16,12 @@ const SalesCard = () => {
 	const [minDate, setMinDate] = useState(min);
 	const [maxDate, setMaxDate] = useState(max);
 
+	// Tipar o useState - informar o tipo que vai ser o dado - Lista de Sale ( Sale[])
+	const [sales, setSales] = useState<Sale[]>([]);
+
 	useEffect(() => {
-		axios.get("http://localhost:8080/sales").then((response) => {
-			console.log(response.data);
+		axios.get(`${BASE_URL}/sales`).then((response) => {
+			setSales(response.data.content);
 		});
 	}, []);
 
@@ -56,45 +61,28 @@ const SalesCard = () => {
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td className="show992px">#341</td>
-							<td className="show576px">08/07/2022</td>
-							<td>Anakin</td>
-							<td className="show992px">15</td>
-							<td className="show992px">11</td>
-							<td>R$ 55300,00</td>
-							<td>
-								<div className="dsmeta-red-btn-container">
-									<NotificationButton />
-								</div>
-							</td>
-						</tr>
-						<tr>
-							<td className="show992px">#341</td>
-							<td className="show576px">08/07/2022</td>
-							<td>Anakin</td>
-							<td className="show992px">15</td>
-							<td className="show992px">11</td>
-							<td>R$ 55300,00</td>
-							<td>
-								<div className="dsmeta-red-btn-container">
-									<NotificationButton />
-								</div>
-							</td>
-						</tr>
-						<tr>
-							<td className="show992px">#341</td>
-							<td className="show576px">08/07/2022</td>
-							<td>Anakin</td>
-							<td className="show992px">15</td>
-							<td className="show992px">11</td>
-							<td>R$ 55300,00</td>
-							<td>
-								<div className="dsmeta-red-btn-container">
-									<NotificationButton />
-								</div>
-							</td>
-						</tr>
+						{sales.map((sale) => {
+							return (
+								<tr key={sale.id}>
+									<td className="show992px">{sale.id}</td>
+									{/* Formatar a data   */}
+									<td className="show576px">
+										{new Date(sale.date).toLocaleDateString()}
+									</td>
+									{/* <td className="show576px">{sale.date}</td> */}
+									<td>{sale.sellerName}</td>
+									<td className="show992px">{sale.visited}</td>
+									<td className="show992px">{sale.deals}</td>
+									{/* Formatar para aparecer 2 casas decimais */}
+									<td>R$ {sale.amount.toFixed(2)}</td>
+									<td>
+										<div className="dsmeta-red-btn-container">
+											<NotificationButton />
+										</div>
+									</td>
+								</tr>
+							);
+						})}
 					</tbody>
 				</table>
 			</div>
