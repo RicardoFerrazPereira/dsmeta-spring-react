1a8cff387cf6c6571ba2ecdc473d31f67d1cee2e
RicardoFerraz <sourvivens@gmail.com>
2022-09-11 11:23:56 -0300

    Enviar notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
 import icon from "../../assets/img/notification-icon.svg";
 
 import "./styles.css";
+import axios from "axios";
+import { BASE_URL } from "../../utils/request";
 
-const NotificationButton = () => {
+type Props = {
+	saleId: number;
+};
+
+const handleClick = (saleId: number) => {
+	axios(`${BASE_URL}/sales/${saleId}/notification`).then((response) => {
+		console.log("Sucesso");
+	});
+};
+
+const NotificationButton = ({ saleId }: Props) => {
 	return (
-		<div className="dsmeta-red-btn">
+		<div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
 			<img src={icon} alt="Notificar" />
 		</div>
 	);
