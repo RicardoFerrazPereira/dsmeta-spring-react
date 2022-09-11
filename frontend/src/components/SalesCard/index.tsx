574bbec8590f52eee6300e955d56f82cb8201f2c
RicardoFerraz <sourvivens@gmail.com>
2022-09-11 10:55:23 -0300

    Atualização Data

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,10 +20,18 @@ const SalesCard = () => {
 	const [sales, setSales] = useState<Sale[]>([]);
 
 	useEffect(() => {
-		axios.get(`${BASE_URL}/sales`).then((response) => {
-			setSales(response.data.content);
-		});
-	}, []);
+		// Passar a data como argumento
+		// toISOString converte a data de um modelo para outro
+		// slice recorta a data - 10 caracteres a partir da posição 0
+		const dmin = minDate.toISOString().slice(0, 10);
+		const dmax = maxDate.toISOString().slice(0, 10);
+
+		axios
+			.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+			.then((response) => {
+				setSales(response.data.content);
+			});
+	}, [minDate, maxDate]);
 
 	return (
 		<div className="dsmeta-card">
